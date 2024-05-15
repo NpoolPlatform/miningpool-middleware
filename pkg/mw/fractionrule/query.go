5425372fbb75ca4b41b8bf15c1948b5b8c7169f5
Geapefurit <670884108@qq.com>
2024-05-15 01:46:45 +0000

    query miningtype and cointype for fractionrule

--- pkg/mw/fractionrule/query.go
+++ pkg/mw/fractionrule/query.go
@@ -4,13 +4,16 @@ import (
 	"context"
 	"fmt"
 
+	"entgo.io/ent/dialect/sql"
 	mpbasetypes "github.com/NpoolPlatform/message/npool/basetypes/miningpool/v1"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 	npool "github.com/NpoolPlatform/message/npool/miningpool/mw/v1/fractionrule"
 
 	"github.com/NpoolPlatform/miningpool-middleware/pkg/db"
 	"github.com/NpoolPlatform/miningpool-middleware/pkg/db/ent"
+	"github.com/NpoolPlatform/miningpool-middleware/pkg/db/ent/coin"
 	fractionruleent "github.com/NpoolPlatform/miningpool-middleware/pkg/db/ent/fractionrule"
+	"github.com/NpoolPlatform/miningpool-middleware/pkg/db/ent/pool"
 
 	fractionrulecrud "github.com/NpoolPlatform/miningpool-middleware/pkg/crud/fractionrule"
 )
@@ -66,6 +69,27 @@ func (h *queryHandler) queryFractionRules(ctx context.Context, cli *ent.Client) 
 	return nil
 }
 
+func (h *queryHandler) queryJoin() {
+	h.stm.Modify(
+		h.queryJoinCoinAndPool,
+	)
+}
+
+func (h *queryHandler) queryJoinCoinAndPool(s *sql.Selector) {
+	coinT := sql.Table(coin.Table)
+	poolT := sql.Table(pool.Table)
+	s.LeftJoin(coinT).On(
+		s.C(fractionruleent.FieldPoolCoinTypeID),
+		coinT.C(coin.FieldEntID),
+	).LeftJoin(poolT).On(
+		coinT.C(coin.FieldPoolID),
+		poolT.C(pool.FieldEntID),
+	).AppendSelect(
+		coinT.C(coin.FieldCoinType),
+		poolT.C(pool.FieldMiningpoolType),
+	)
+}
+
 func (h *queryHandler) scan(ctx context.Context) error {
 	return h.stm.Scan(ctx, &h.infos)
 }
@@ -86,6 +110,7 @@ func (h *Handler) GetFractionRule(ctx context.Context) (*npool.FractionRule, err
 		if err := handler.queryFractionRule(cli); err != nil {
 			return err
 		}
+		handler.queryJoin()
 		const singleRowLimit = 2
 		handler.stm.Offset(0).Limit(singleRowLimit)
 		return handler.scan(_ctx)
@@ -113,6 +138,7 @@ func (h *Handler) GetFractionRules(ctx context.Context) ([]*npool.FractionRule, 
 		if err := handler.queryFractionRules(ctx, cli); err != nil {
 			return err
 		}
+		handler.queryJoin()
 		handler.stm.
 			Offset(int(h.Offset)).
 			Limit(int(h.Limit)).
